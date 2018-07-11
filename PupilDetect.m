4e69ca6c31d6239a34023b98123040f4ce1f4994
Byron Price <byron.h.price@gmail.com>
2018-07-11 16:06:01 -0400

    add more info for use

--- PupilDetect.m
+++ PupilDetect.m
@@ -1,6 +1,17 @@
 function [] = PupilDetect(filename)
-% function to take .avi file and output pupil center location and pupil
-% area
+% Function to take .avi file of a mouse's face and output pupil center 
+%  location, pupil area, and pupil diameter. The algorithm is a combination 
+%  of the starburst and a luminance threshold algorithm.
+
+%  there are 2 key free parameters that will depend on luminance conditions
+%  in your setup
+%    edgeThreshold - for starburst algorithm, the luminance difference that
+%       will trigger the algorithm to break as it creates its rays [ this
+%       will be minimum luminance difference that we expect at the edge of
+%       the pupil (light - dark) ]
+%    luminanceThreshold - luminance threshold to help find dark pixels
+%      corresponding to the pupil, this should be a little bit less than
+%      the average luminance of pupil pixels
 
 %INPUT: filename - .avi file recorded from eye-tracker
 %
@@ -8,7 +19,7 @@ function [] = PupilDetect(filename)
 
 %CREATED: 2018/07/10
 %  Byron Price
-%UPDATED: 2018/07/10
+%UPDATED: 2018/07/11
 % By: Byron Price
 
 % filename = 'EyeTracker_20180709-6028141.avi';
