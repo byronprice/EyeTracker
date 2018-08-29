a73de6d723744a052a3096de7662a30d30b5c415
Byron Price <byron.h.price@gmail.com>
2018-08-28 20:57:52 -0400

    Update InitializePupilDetection.m

--- InitializePupilDetection.m
+++ InitializePupilDetection.m
@@ -23,10 +23,7 @@ function [] = InitializePupilDetection(filename)
 % By: Byron Price
 
 v = VideoReader(filename);
-N = ceil(v.Duration*v.FrameRate);
 
-edgeThreshold = 4;% THE MAIN FREE PARAMETERS
-luminanceThreshold = 46;
 if hasFrame(v)
     im = readFrame(v);
     im = mean(im,3);
@@ -43,49 +40,24 @@ if hasFrame(v)
     [X,Y] = getpts;
     pupilCenterEst = [X,Y];
     
-    imshow(uint8(tmp));caxis([30 150]);
-    title('Click a bunch of points in pupil');
-    [X,Y] = getpts;
     pupilLuminance = [];
-    for ii=1:length(X)
-        pupilLuminance = [pupilLuminance;tmp(round(Y(ii)),round(X(ii)))];
+    for jj=1:5
+        v.CurrentTime = (v.Duration-1).*rand;
+        im = readFrame(v);
+        
+        tmp = im(minY:maxY,minX:maxX);
+        
+        imshow(uint8(tmp));caxis([30 150]);
+        title('Click a bunch of points in pupil');
+        [X,Y] = getpts;
+        for ii=1:length(X)
+            pupilLuminance = [pupilLuminance;tmp(round(Y(ii)),round(X(ii)))];
+        end
     end
-    
-    imshow(uint8(tmp));caxis([30 150]);
-    title('Click a bunch of points in grey area of eye (outside pupil)');
-    [X,Y] = getpts;
-    greyLuminance = [];
-    for ii=1:length(X)
-        greyLuminance = [greyLuminance;tmp(round(Y(ii)),round(X(ii)))];
-    end
-    
-    
-    v.CurrentTime = 3*v.Duration/4;
-    im = readFrame(v);
-    
-    tmp = im(minY:maxY,minX:maxX);
-    
-    imshow(uint8(tmp));caxis([30 150]);
-    title('Click a bunch of points in pupil');
-    [X,Y] = getpts;
-    pupilLuminance = [];
-    for ii=1:length(X)
-        pupilLuminance = [pupilLuminance;tmp(round(Y(ii)),round(X(ii)))];
-    end
-    
-    imshow(uint8(tmp));caxis([30 150]);
-    title('Click a bunch of points in grey area of eye (outside pupil)');
-    [X,Y] = getpts;
-     greyLuminance = [];
-    for ii=1:length(X)
-        greyLuminance = [greyLuminance;tmp(round(Y(ii)),round(X(ii)))];
-    end
-    
     pupilLuminance = double(pupilLuminance);
-    greyLuminance = double(greyLuminance);
     
     luminanceThreshold = mean(pupilLuminance)+2*std(pupilLuminance);
-    edgeThreshold = max(3*std(pupilLuminance),4);
+    edgeThreshold = round(min(max(3*std(pupilLuminance),4),10));
     
 
     filename = filename(1:end-4);
