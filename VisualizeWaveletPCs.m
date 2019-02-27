25f33d48020a1b186d88fffc0089dbc15968f2af
byronprice <byron.h.price@gmail.com>
2019-02-27 16:41:54 -0500

    real-time PCA to use full set of wavelets

--- VisualizeWaveletPCs.m
+++ VisualizeWaveletPCs.m
@@ -3,15 +3,13 @@
 load('EyeTracker_20180806-4026621-wvltpca.mat');
 
 x = zeros(DIM);
-[C,S] = wavedec2(x,wvltLevel,wvltType);
+[~,S] = wavedec2(x,wvltLevel,wvltType);
 cm = 'bone';
 % cm = magma(100);
 figure;
 for ii=1:20
-    tmp = Winv(ii,:);
-    C = zeros(size(C));
-    C(keptInds) = tmp;
-    I = appcoef2(C,S,wvltType,1);
+    C = Winv(ii,:);
+    I = appcoef2(C,S,wvltType,0);
     subplot(5,4,ii);imagesc(I);colormap(cm);
     ax = gca;set(ax,'xtick',[]);set(ax,'ytick',[]);
     title(sprintf('PC %d',ii));
