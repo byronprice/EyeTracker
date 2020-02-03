1c451c6f21578f2b1222d7acb16f09e1563ce17e
Byron Price <byron.h.price@gmail.com>
2020-02-03 14:21:27 -0500

    motion pca and motion ica

---
+++ WaveletICA.m
@@ -0,0 +1,100 @@
+function [] = WaveletICA(filename,imInds)
+%WaveletICA.m
+%   Detailed explanation goes here
+savefilename = filename(1:end-4);
+savefilename = strcat(savefilename,'-wvltica.mat');
+
+if exist(savefilename,'file') == 2
+    return;
+else
+    v = VideoReader(filename);
+    totalFrames = round(v.Duration*v.FrameRate);
+    im = readFrame(v);
+    
+    if nargin<2
+        DIM = size(im);
+        imInds = [1,DIM(1),1,DIM(2)];
+        im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+    else
+        im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+        DIM = size(im);
+    end
+    
+    % vidTime = 250/1000;
+    % numFrames = ceil(vidTime/(1/v.FrameRate));
+    % video = zeros(DIM(1),DIM(2),numFrames);
+    % WDEC = wavedec3(video,5,'db2');
+    % N = size(WDEC.dec,1);
+    % all = [];
+    % for ii=1:N
+    %     all = [all;WDEC.dec{ii}(:)];
+    % end
+    %
+    % fullSize = length(all);
+    
+    wvltLevel = 1;
+    wvltType = 'db6';
+    [C,S] = wavedec2(im,wvltLevel,wvltType);
+    fullSize = length(C(:));
+    
+    % ica
+    numICA = min(1e5,ceil(totalFrames/2));
+    times = randperm(totalFrames,numICA);
+    
+    q = 500;
+    fprintf('Data Size: [%d , %d]\n',fullSize,numICA);
+    data = zeros(fullSize,numICA);
+    
+    count = 0;
+    while count<numICA
+        v.CurrentTime = (times(count+1)-1)./v.FrameRate;
+        im = readFrame(v);
+        
+        if hasFrame(v)
+            count = count+1;
+            im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+            [C,~] = wavedec2(im,wvltLevel,wvltType);
+            
+            data(:,count) = C(:);
+        else
+            times(count+1) = randperm(totalFrames,1); 
+        end
+    end
+    fprintf('Computing ICA ... \n');
+    [ W, Tinv,T, mu] = kICA(data,q);
+    fprintf('ICA Complete ... \n');
+    WTinv = W*Tinv;
+    TWt = T*W';
+
+    clear T Tinv W;    
+    % TRANSFORM ALL OF THE DATA INTO IC SPACE
+    clear v data;
+    v = VideoReader(filename);
+    totalFrames = ceil(v.Duration*v.FrameRate);
+    
+    icaRep = zeros(totalFrames,q);
+    
+    for ii=1:totalFrames
+        if hasFrame(v)
+            im = readFrame(v);
+            im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+            [C,~] = wavedec2(im,wvltLevel,wvltType);
+            icaRep(ii,:) = (WTinv*(C(:)-mu))'; % to go the other way T*W'*icaRep'+mu
+        end
+    end
+    
+    if sum(icaRep(end,:))<=1e-6
+        icaRep = icaRep(1:end-1,:);
+    end
+    
+    clear v;
+    
+    newName = filename(1:end-4);
+    newName = strcat(newName,'-wvltica.mat');
+    save(newName,'icaRep','WTinv','TWt','mu','q','wvltLevel','wvltType',...
+        'DIM','filename','S','imInds');
+    
+    disp(['File Completed: ',filename]);
+    
+end
+end
--- WaveletPCAMot.m
+++ WaveletPCAMot.m
@@ -1,8 +1,8 @@
-function [] = WaveletPCA(filename,imInds)
+function [] = WaveletPCAMot(filename,imInds)
 %WaveletPCA.m
 %   Detailed explanation goes here
 savefilename = filename(1:end-4);
-savefilename = strcat(savefilename,'-wvltpca.mat');
+savefilename = strcat(savefilename,'-wvltpcamot.mat');
 
 if exist(savefilename,'file') == 2
     return;
@@ -42,65 +42,69 @@ else
     times = randperm(totalFrames,numPCA);
     
     q = 500;
-    W = normrnd(0,1,[fullSize,q]);
-    eigenvalues = zeros(q,1);
-    mu = zeros(fullSize,1);
+    fprintf('Data Size: [%d , %d]\n',fullSize,numPCA);
+    data = zeros(fullSize,numPCA);
     
-    step = max(min(0.1,1./(1:numPCA)),1e-4);
-    for tt=1:numPCA
-        v.CurrentTime = (times(tt)-1)./v.FrameRate;
+    count = 0;
+    while count<numPCA
+        v.CurrentTime = (times(count+1)-1)./v.FrameRate;
         im = readFrame(v);
-        im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
-        [C,~] = wavedec2(im,wvltLevel,wvltType);
-        meanSubtract = C(:)-mu;
-        mu = mu+(1/(tt+1))*meanSubtract;
         
-        if tt>100 && mod(tt,10)==0
-            phi = meanSubtract'*W;
-            W = W+step(tt)*meanSubtract*phi;
-            W = GramSchmidt(W);
-            eigenvalues = eigenvalues+step(tt)*((phi').^2-eigenvalues);
+        if hasFrame(v)
+            count = count+1;
+            im2 = readFrame(v);
+            im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+            im2 = mean(im2(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+            [C,~] = wavedec2(im2-im,wvltLevel,wvltType);
+            
+            data(:,count) = C(:);
+        else
+            times(count+1) = randperm(totalFrames,1); 
         end
     end
-    
-    [~,inds] = sort(eigenvalues);
-    
-    eigenvalues = eigenvalues(inds);
-    
-    newW = zeros(size(W));
-    
-    for ii=1:q
-        newW(:,ii) = W(:,inds(ii));
-    end
-    W = newW;
-    
-    Winv = pinv(W);
-    
-    % TRANSFORM ALL OF THE DATA INTO PC SPACE
-    clear v;
+    fprintf('Computing PCA ... \n');
+    [W,mu] = PCA(data,q);
+    fprintf('PCA Complete ... \n');
+    Winv = W';
+    % TRANSFORM ALL OF THE DATA INTO IC SPACE
+    clear v data;
     v = VideoReader(filename);
     totalFrames = ceil(v.Duration*v.FrameRate);
     
     pcaRep = zeros(totalFrames,q);
-    for ii=1:totalFrames
+    
+    meanIm = appcoef2(mu,S,wvltType,0);
+    im = readFrame(v);
+    im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
+    [C,~] = wavedec2(im-meanIm,wvltLevel,wvltType);
+    pcaRep(1,:) = (Winv*(C(:)-mu))'; % to go the other way W*pcaRep'+mu
+    prevIm = im;
+    
+    checks = round(linspace(2,totalFrames,20));checkcount=1;    
+    for ii=2:totalFrames
         if hasFrame(v)
             im = readFrame(v);
             im = mean(im(imInds(1):imInds(2),imInds(3):imInds(4)),3);
-            [C,~] = wavedec2(im,wvltLevel,wvltType);
+            [C,~] = wavedec2(im-prevIm,wvltLevel,wvltType);
             pcaRep(ii,:) = (Winv*(C(:)-mu))';
+            prevIm = im;
         end
+	if checks(checkcount)==ii
+	    fprintf('%3.2f Percent Complete\n',100*ii/totalFrames);
+            checkcount = checkcount+1;
+	end
     end
     
-    if sum(pcaRep(end,:))==0
+    if sum(pcaRep(end,:))<=1e-6
         pcaRep = pcaRep(1:end-1,:);
     end
     
     clear v;
     
     newName = filename(1:end-4);
-    newName = strcat(newName,'-wvltpca.mat');
+    newName = strcat(newName,'-wvltpcamot.mat');
     save(newName,'pcaRep','W','Winv','mu','q','wvltLevel','wvltType',...
-        'DIM','filename','eigenvalues','S','imInds');
+        'DIM','filename','S','imInds');
     
     disp(['File Completed: ',filename]);
     
@@ -112,11 +116,12 @@ function [Q] = GramSchmidt(A)
 Q = zeros(M,N);
 R = zeros(N,N);
 
+v = zeros(M,1);
 for jj=1:N
-    v = A(:,jj);
+    v(:) = A(:,jj);
     for ii=1:jj-1
        R(ii,jj) = Q(:,ii)'*v;
-       v = v-R(ii,jj)*Q(:,ii);
+       v(:) = v-R(ii,jj)*Q(:,ii);
     end
     R(jj,jj) = norm(v); % v'*v
     Q(:,jj) = v./R(jj,jj);
